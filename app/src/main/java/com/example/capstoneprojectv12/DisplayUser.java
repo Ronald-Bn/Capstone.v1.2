9b9a0e13ba6ab58752efddc7eaf0e3a6c81aa0d4
Ronald-bn <ronaldbabailan0026@gmail.com>
2021-08-07 08:24:30 +0800

    CapstoneProject.v.1

--- app/src/main/java/com/example/capstoneprojectv12/DisplayUser.java
+++ app/src/main/java/com/example/capstoneprojectv12/DisplayUser.java
@@ -2,13 +2,33 @@ package com.example.capstoneprojectv12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayUser extends AppCompatActivity {
 
+    TextView setTextFullName, setTextPhoneNum, setTextEmail, setTextPassword, setTextAddress, setTextZipCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_user);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("phone");
+        setTextFullName = findViewById(R.id.SetTextFullName);
+        setTextPhoneNum = findViewById(R.id.SetTextPhoneNum);
+        setTextEmail = findViewById(R.id.SetTextEmail);
+        setTextPassword = findViewById(R.id.SetTextPassword);
+        setTextAddress = findViewById(R.id.SetTextAddress);
+        setTextZipCode = findViewById(R.id.SetTextZipCode);
+
+        User user = new User();
+        String phone = user.getPhonenum();
+        Toast.makeText(this, ""+ str, Toast.LENGTH_SHORT).show();
+        setTextFullName.setText(user.getFullname());
+        setTextPhoneNum.setText(user.getPhonenum());
+
     }
 }
--- app/src/main/java/com/example/capstoneprojectv12/Register.java
+++ app/src/main/java/com/example/capstoneprojectv12/Register.java
@@ -2,6 +2,7 @@ package com.example.capstoneprojectv12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
@@ -40,40 +41,29 @@ public class Register extends AppCompatActivity {
         });
 
        btn_register.setOnClickListener(v->
-        {
-            if(TextUtils.isEmpty(edit_fullname.getText())){
-                Toast.makeText(this, "Please Enter Your Full Name", Toast.LENGTH_SHORT).show();
-            }else if(TextUtils.isEmpty(edit_phonenum.getText()) || edit_phonenum.getText().length() != 11 ){
-                Toast.makeText(this, "Please Enter Your Phone Number", Toast.LENGTH_SHORT).show();
-            }else if(TextUtils.isEmpty(edit_email.getText())){
-                Toast.makeText(this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
-            }else if(TextUtils.isEmpty(edit_password.getText())){
-                Toast.makeText(this, "Please Enter Your Password", Toast.LENGTH_SHORT).show();
-            }else if(TextUtils.isEmpty(edit_confirmpassword.getText())){
-                Toast.makeText(this, "Please Enter Your Confirm Password", Toast.LENGTH_SHORT).show();
-            }else if(TextUtils.isEmpty(edit_address.getText())){
-                Toast.makeText(this, "Please Enter Your Address", Toast.LENGTH_SHORT).show();
-            }else if(TextUtils.isEmpty(edit_zipcode.getText())){
-                Toast.makeText(this, "Please Enter Your Zip Code", Toast.LENGTH_SHORT).show();
-            }else if(!edit_password.getText().toString().trim().equals(edit_confirmpassword.getText().toString().trim())){
-                Toast.makeText(this, "The Password is Not Match", Toast.LENGTH_SHORT).show();
-            }
-                else {
-                User users = new User(edit_fullname.getText().toString(), edit_phonenum.getText().toString(), edit_email.getText().toString(), edit_password.getText().toString(), edit_confirmpassword.getText().toString(), edit_address.getText().toString(), edit_zipcode.getText().toString());
-                dao.add(users).addOnSuccessListener(suc ->
-                {
-                    edit_fullname.setText("");
-                    edit_address.setText("");
-                    edit_phonenum.setText("");
-                    edit_password.setText("");
-                    edit_zipcode.setText("");
-                    edit_email.setText("");
-                    edit_confirmpassword.setText("");
-                    Toast.makeText(this, "Record is inserted", Toast.LENGTH_SHORT).show();
-                }).addOnFailureListener(er ->
-                        Toast.makeText(this, "" + er.getMessage(), Toast.LENGTH_SHORT).show());
-
-            }
-        });
-    }
+       {
+           if (TextUtils.isEmpty(edit_fullname.getText())) {
+               Toast.makeText(this, "Please Enter Your Full Name", Toast.LENGTH_SHORT).show();
+           } else if (TextUtils.isEmpty(edit_phonenum.getText()) || edit_phonenum.getText().length() != 11) {
+               Toast.makeText(this, "Please Enter Your Phone Number", Toast.LENGTH_SHORT).show();
+           } else if (TextUtils.isEmpty(edit_email.getText())) {
+               Toast.makeText(this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+           } else if (TextUtils.isEmpty(edit_password.getText())) {
+               Toast.makeText(this, "Please Enter Your Password", Toast.LENGTH_SHORT).show();
+           } else if (TextUtils.isEmpty(edit_confirmpassword.getText())) {
+               Toast.makeText(this, "Please Enter Your Confirm Password", Toast.LENGTH_SHORT).show();
+           } else if (TextUtils.isEmpty(edit_address.getText())) {
+               Toast.makeText(this, "Please Enter Your Address", Toast.LENGTH_SHORT).show();
+           } else if (TextUtils.isEmpty(edit_zipcode.getText())) {
+               Toast.makeText(this, "Please Enter Your Zip Code", Toast.LENGTH_SHORT).show();
+           } else if (!edit_password.getText().toString().trim().equals(edit_confirmpassword.getText().toString().trim())) {
+               Toast.makeText(this, "The Password is Not Match", Toast.LENGTH_SHORT).show();
+           } else {
+               User users = new User(edit_fullname.getText().toString(), edit_phonenum.getText().toString(), edit_email.getText().toString(), edit_password.getText().toString(), edit_confirmpassword.getText().toString(), edit_address.getText().toString(), edit_zipcode.getText().toString());
+               users.setPhonenum(edit_phonenum.getText().toString().trim());
+               Toast.makeText(this, "" + users.getPhonenum(), Toast.LENGTH_SHORT).show();
+               Toast.makeText(this, "Record is inserted", Toast.LENGTH_SHORT).show();
+           }
+           });
+       }
 }
